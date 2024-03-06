0f5009b22868998f3d7e95ca26dfee157b17d3d3
kiamaria <kia@hietamaki.org>
2024-03-06 16:56:44 +0200

    Valmis

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar, useState } from "expo-status-bar";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+import * as Speech from "expo-speech";
 
 export default function App() {
+  const [speech, setSpeech] = React.useState("");
+
+  const speak = () => {
+    Speech.speak(speech);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setSpeech(text)}
+      />
+      <Button title="Press to hear text" onPress={speak} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
